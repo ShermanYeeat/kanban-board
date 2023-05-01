d89ba82d39eee7eb7c01a9dba378782afaf8819b
ShermanYeeat <shermanyee11@gmail.com>
2023-05-01 01:00:33 -0700

    lint

--- src/components/TicketModal.tsx
+++ src/components/TicketModal.tsx
@@ -1,26 +1,24 @@
-
-
 import { type RouterOutputs } from "../utils/api";
 
 type Ticket = RouterOutputs["ticket"]["getAll"][0];
 
-export const TicketModal = ({
-  ticket,
-}: {
-  ticket: Ticket;
-}) => {
+export const TicketModal = ({ ticket }: { ticket: Ticket }) => {
   // const [isOpen, setIsOpen] = useState<boolean>(true);
 
   return (
-    <input type="checkbox" id="my-modal" className="modal-toggle" />
-    <div className="modal">
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-        <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-        <div className="modal-action">
-          <label htmlFor="my-modal" className="btn">Yay!</label>
+    <>
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">header</h3>
+          <p className="py-4">paragraph</p>
+          <div className="modal-action">
+            <label htmlFor="my-modal" className="btn">
+              label
+            </label>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
